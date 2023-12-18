faad7e7cd036f38b98cacb7d37c49570eafeb8af
Cyrel Cabodbod <mariagejaks@gmail.com>
2023-12-18 10:43:05 +0800

    add avatar

--- dashboard_student.php
+++ dashboard_student.php
@@ -89,6 +89,39 @@ header("Content-Security-Policy: img-src * data:");
         .custom-input:focus {
             box-shadow: none !important;
         }
+
+       
+        .avatar-container {
+        position: relative;
+    }
+
+    .avatar-container img {
+        width: 100%;
+        height: auto;
+    }
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba(0, 0, 0, 0.8);
+        border-radius: 50%;
+    }
+
+    .avatar-container:hover .overlay {
+        display: flex;
+    }
+
+    .overlay span {
+        color: rgb(250, 250, 250);
+        margin-top: 5px;
+    }
     </style>
 
     <!--stylesheet using bootstrap5-->
@@ -145,7 +178,20 @@ header("Content-Security-Policy: img-src * data:");
     <div class="container-fluid">
         <div class="row text-white">
             <div class="col-3 fixed-column">
-                <i class="far fa-user-circle"></i>
+            <div class="d-flex flex-column align-items-center">
+        <input id="file" type="file" onchange="uploadImage(event)" style="display:none;" />
+        <div class="avatar-container" onclick="document.getElementById('file').click();">
+            <?php
+            $avatarSrc = isset($_SESSION['avatar']) ? $_SESSION['avatar'] : 'image/profile.jpg';
+            echo '<img src="' . $avatarSrc . '" class="rounded-circle" style="width: 150px;" alt="Avatar" />';
+            ?>
+            <div class="overlay">
+                <!-- Label and camera icon content goes here -->
+                <i class="fa fa-camera" style="font-size: 14px;"></i>
+                <span>Change Image</span>
+            </div>
+        </div>
+    </div>
                 <h6 class="pt-4 px-5">Name: <?php echo $_SESSION['users_name'] ?></h6>
                 <h6 class="px-5">ID Number: <?php echo $_SESSION['idNumberStudent'] ?></h6>
             </div>
